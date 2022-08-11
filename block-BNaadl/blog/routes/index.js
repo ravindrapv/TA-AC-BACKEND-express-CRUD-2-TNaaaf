8f62e01e0dceec8d34e2cb976b4fc8ab93bf4108
ravindrapv <ravindra.ravi4849@gmail.com>
2022-08-11 21:29:51 +0530

    delete

--- block-BNaadl/blog/routes/index.js
+++ block-BNaadl/blog/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var Blog = require('../models/blog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('welcome', { title: 'Express' });
 });
 
 module.exports = router;
